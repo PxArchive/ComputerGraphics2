669f5851c04223c6c52b18c2dc852f92c41f8748
PxArchive <peterriemerbjorn@gmail.com>
2025-04-09 15:23:10 +0200

    Fixed orb lighting

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class PlayerManager : MonoBehaviour
 {
@@ -9,20 +11,75 @@ public class PlayerManager : MonoBehaviour
     public bool grounded = true;
     public bool doJump = false;
 
+    public float orbTimer = 0f;
+    public float totalOrbActiveTime = 5f;
+    private float orbCurrentScale = 0.1f;
+    private float orbTargetScale = 0.1f;
+    private float orbScaleVelocity  = 0f;
+    public float orbScaleSmoothTime = 0.2f;
+
+    float vignetteValue = 0f;
+    float vignetteTarget = 0f;
+    float vignetteVelocity = 0f;
+    public float vignetteSmoothTime = 0.2f;
+
     private Rigidbody rb;
+    private Transform orb;
+    private Volume volume;
+
+    Vignette vignette;
+
 
     private Vector3 vectorZERO = Vector3.zero;
+    private Vector3 vectorONE = Vector3.one;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        orb = GameObject.Find("Player_ORB").transform;
+        volume = GameObject.Find("VOLUME").GetComponent<Volume>();
+        volume.profile.TryGet(out vignette);
+
+        Application.targetFrameRate = 60;
+        Screen.SetResolution(
+            Screen.currentResolution.width,
+            Screen.currentResolution.height,
+            FullScreenMode.Windowed,
+            new RefreshRate()
+            {
+                numerator = 60000,
+                denominator = 1001
+            });
     }
 
     // Update is called once per frame
     void Update()
     {
         //transform.position += new Vector3(inputDirection.x, 0f, inputDirection.y) * walkSpeed;
+
+        if (orbTimer > 0)
+        {
+            orbTimer -= Time.deltaTime;
+
+            orbTargetScale = 5f;
+            vignetteTarget = 0.45f;
+        }
+        else
+        {
+            orbTargetScale = 0.1f;
+            vignetteTarget = 0f;
+        }
+
+        orbCurrentScale = Mathf.SmoothDamp(orbCurrentScale, orbTargetScale, ref orbScaleVelocity, orbScaleSmoothTime);
+
+        orb.transform.localScale = vectorONE * orbCurrentScale;
+
+        if (vignette)
+        {
+            vignetteValue = Mathf.SmoothDamp(vignetteValue, vignetteTarget, ref vignetteVelocity, vignetteSmoothTime);
+            vignette.intensity.SetValue(new ClampedFloatParameter(vignetteValue, 0f, 1f, true));
+        }
     }
 
     private void FixedUpdate()
@@ -70,6 +127,14 @@ public class PlayerManager : MonoBehaviour
         if (_input.isPressed && grounded)
         {
             doJump = true;
+        }
+    }
+
+    public void OnInteract(InputValue _input)
+    {
+        if (orbTimer <= 0)
+        {
+            orbTimer = totalOrbActiveTime;
         }
     }
 
