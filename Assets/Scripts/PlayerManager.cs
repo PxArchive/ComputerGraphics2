d13e47e2d1efb5a6ac649b4fa47fc73f8a1f6d91
PxArchive <peterriemerbjorn@gmail.com>
2025-04-11 15:36:15 +0200

    Added ghost object shader

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,3 +1,4 @@
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
@@ -12,23 +13,28 @@ public class PlayerManager : MonoBehaviour
     public bool doJump = false;
 
     public float orbTimer = 0f;
-    public float totalOrbActiveTime = 5f;
+    public float totalOrbActiveTime = 3.5f;
     private float orbCurrentScale = 0.1f;
     private float orbTargetScale = 0.1f;
+    private const float maxOrbScale = 10f;
+    private const float minOrbScale = 0.1f;
     private float orbScaleVelocity  = 0f;
     public float orbScaleSmoothTime = 0.2f;
 
-    float vignetteValue = 0f;
-    float vignetteTarget = 0f;
-    float vignetteVelocity = 0f;
+    //float vignetteValue = 0f;
+    //float vignetteTarget = 0f;
+    //float vignetteVelocity = 0f;
     public float vignetteSmoothTime = 0.2f;
+
+    private float globalTValue = 0f;
 
     private Rigidbody rb;
     private Transform orb;
     private Volume volume;
 
     Vignette vignette;
-
+    DepthOfField dof;
+    CinemachineCamera cineCam;
 
     private Vector3 vectorZERO = Vector3.zero;
     private Vector3 vectorONE = Vector3.one;
@@ -38,8 +44,10 @@ public class PlayerManager : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         orb = GameObject.Find("Player_ORB").transform;
+        cineCam = FindFirstObjectByType<CinemachineCamera>();
         volume = GameObject.Find("VOLUME").GetComponent<Volume>();
         volume.profile.TryGet(out vignette);
+        volume.profile.TryGet(out dof);
 
         Application.targetFrameRate = 60;
         Screen.SetResolution(
@@ -62,23 +70,28 @@ public class PlayerManager : MonoBehaviour
         {
             orbTimer -= Time.deltaTime;
 
-            orbTargetScale = 5f;
-            vignetteTarget = 0.45f;
+            orbTargetScale = maxOrbScale;
+            //vignetteTarget = 0.45f;
         }
         else
         {
-            orbTargetScale = 0.1f;
-            vignetteTarget = 0f;
+            orbTargetScale = minOrbScale;
+            //vignetteTarget = 0f;
         }
 
         orbCurrentScale = Mathf.SmoothDamp(orbCurrentScale, orbTargetScale, ref orbScaleVelocity, orbScaleSmoothTime);
 
         orb.transform.localScale = vectorONE * orbCurrentScale;
 
-        if (vignette)
+        globalTValue = Mathf.InverseLerp(minOrbScale, maxOrbScale, orbCurrentScale);
+
+        if (vignette && dof && cineCam)
         {
-            vignetteValue = Mathf.SmoothDamp(vignetteValue, vignetteTarget, ref vignetteVelocity, vignetteSmoothTime);
-            vignette.intensity.SetValue(new ClampedFloatParameter(vignetteValue, 0f, 1f, true));
+            //vignetteValue = Mathf.SmoothDamp(vignetteValue, vignetteTarget, ref vignetteVelocity, vignetteSmoothTime);
+            vignette.intensity.SetValue(new ClampedFloatParameter(Mathf.Lerp(0f, 0.25f, globalTValue), 0f, 1f, true));
+            dof.focusDistance.SetValue(new ClampedFloatParameter(7, 0.1f, 1000f, true));
+            dof.focalLength.SetValue(new ClampedFloatParameter(Mathf.Lerp(1f, 74f, globalTValue), 1f, 300f, true));
+            cineCam.Lens.FieldOfView = Mathf.Lerp(60f, 55f, globalTValue);
         }
     }
 
